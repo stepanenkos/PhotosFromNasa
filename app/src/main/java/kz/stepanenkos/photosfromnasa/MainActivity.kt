7466dd3eb10fd5c804a531d3525d8fbd7de67f8b
Sergey Stepanenko <ssv2408@gmail.com>
2021-12-13 00:32:44 +0600

    Refactor

--- app/src/main/java/kz/stepanenkos/photosfromnasa/MainActivity.kt
+++ app/src/main/java/kz/stepanenkos/photosfromnasa/MainActivity.kt
@@ -3,9 +3,7 @@ package kz.stepanenkos.photosfromnasa
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import kz.stepanenkos.photosfromnasa.databinding.ActivityMainBinding
 
@@ -19,17 +17,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val bottomNavigationView: BottomNavigationView = binding.navView
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragmentActivityMain.id) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_pictures_of_the_days, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+         bottomNavigationView.setupWithNavController(navController)
     }
 }
