13ffb9ffc3f7574b3ec7be4458eeecc9b438b56d
Sergey Stepanenko <ssv2408@gmail.com>
2021-12-13 00:31:06 +0600

    Добавлен RecyclerView в фрагмент

--- app/src/main/java/kz/stepanenkos/photosfromnasa/allDates/presentation/PicturesOfTheDaysFragment.kt
+++ app/src/main/java/kz/stepanenkos/photosfromnasa/allDates/presentation/PicturesOfTheDaysFragment.kt
@@ -7,12 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kz.stepanenkos.photosfromnasa.allDates.presentation.view.PicturesAdapter
 import kz.stepanenkos.photosfromnasa.databinding.FragmentPicturesOfTheDaysBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PicturesOfTheDaysFragment : Fragment() {
     private val viewModel by viewModel<PicturesOfTheDaysViewModel>()
-
+    private lateinit var recyclerView: RecyclerView
+    private val picturesAdapter = PicturesAdapter()
     private var _binding: FragmentPicturesOfTheDaysBinding? = null
 
     // This property is only valid between onCreateView and
@@ -26,9 +30,14 @@ class PicturesOfTheDaysFragment : Fragment() {
     ): View {
 
         _binding = FragmentPicturesOfTheDaysBinding.inflate(inflater, container, false)
+        recyclerView = binding.picturesRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = picturesAdapter
+
         viewModel.getAllDates()
         viewModel.text.observe(viewLifecycleOwner) {
             Log.d("TAG", "$it")
+            picturesAdapter.submitList(it)
         }
         viewModel.error.observe(viewLifecycleOwner) {
             Log.d("TAG", "$it")
